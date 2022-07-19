1e0cb01cc7d00a10a03748c7fdcb4d87046e58ed
Jay Slade <brandonslade2@gmail.com>
2022-07-18 21:15:50 -0700

    hasUniqueChars finish

--- hasUniqueChars.js
+++ hasUniqueChars.js
@@ -1,1 +1,17 @@
-// Write your code below
+// Write your code below+
+
+const hasUniqueChars = (word) => {
+    let uniquechars = new Set([])
+    for (i = 0; i < word.length; i++) {
+        uniquechars.add(word[i])
+
+    }
+    return word.length === uniquechars.size
+}
+
+//learned that the .add method does not add duplicates
+//learned a new method. .size  counts how many indexes
+// learned here that strings have their own indexes. wow that would be good to know.
+// leanred you can temporarily make a new object
+
+console.log(hasUniqueChars("monday"))
